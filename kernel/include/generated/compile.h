ed53841ab6214bf76e018186f0332701db28be8b
banjo0917 <rymoj0917@yahoo.com>
2013-12-22 23:04:55 +0800

    First Boot

---
+++ kernel/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 4 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 SMP PREEMPT Sun Dec 22 18:40:00 PHT 2013"
+#define LINUX_COMPILE_BY "banjo0917"
+#define LINUX_COMPILE_HOST "banjo0917-virtualbox"
+#define LINUX_COMPILER "gcc version 4.6.3 (GCC) "
